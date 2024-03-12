24e0ed1d74f5d226a1c45d20f17912f00068433a
Rami-CUD <20220001825@students.cud.ac.ae>
2024-03-12 12:50:50 +0400

    Unhardcoded API KEY

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,11 @@ from langchain_google_genai import GoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
 
 if "GOOGLE_API_KEY" not in os.environ:
-    # os.environ["GOOGLE_API_KEY"] = getpass.getpass("API Key: ")
-    os.environ["GOOGLE_API_KEY"] = "AIzaSyCILLp4kYKQKVW8BWmXE2Hh4fomiZwXdfU"
+    API_KEY = st.sidebar.text_input('OpenAI API Key', type='password')
 
 st.title = "Testing"
 
-model = GoogleGenerativeAI(model="gemini-pro")
+model = GoogleGenerativeAI(model="gemini-pro", google_api_key=API_KEY)
 
 with st.form("my_form"):
     text = st.text_area("Enter Text")
