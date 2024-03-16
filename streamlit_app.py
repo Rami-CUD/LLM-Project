bdef6efd76fda94bd63dcfc3bbe1dc9182fa6580
Rami-CUD <20220001825@students.cud.ac.ae>
2024-03-16 19:35:16 +0400

    Commented out popover and printed out st version to check streamlit cloud st version (Debugging)

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,11 @@ if "stack" not in st.session_state or "key" not in st.session_state:
     
 st.session_state.key = "AIzaSyCILLp4kYKQKVW8BWmXE2Hh4fomiZwXdfU"
 st.title = "Testing"
-
+with st.echo():
+    st.write(st.__version__)
 model = GoogleGenerativeAI(model="gemini-pro", google_api_key=st.session_state.key)
-with st.popover("Upload a File"):
-    st.file_uploader(label="dds", type=".pdf", label_visibility="collapsed")
+# with st.popover("Upload a File"):
+#     st.file_uploader(label="dds", type=".pdf", label_visibility="collapsed")
 messages = st.container()
 chat = st.chat_input("Enter Text")
 if chat:
